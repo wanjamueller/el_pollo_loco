dc121c968df7a114cd1b60fda6d7fb1bbf19efd6
wanjamueller <me@wanjamueller.com>
2026-08-31 23:34:22 +0200

    endboss starts moving and attacks

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -2,7 +2,6 @@ import { level1 } from "../levels/level1.js";
 import { Imagehub } from "./image-hub.class.js";
 import { IntervalHub } from "./intervallhub.class.js";
 import { MovableObject } from "./movable-object.class.js";
-import { World } from "./world.class.js";
 
 export class Endboss extends MovableObject {
     y = 230;
@@ -17,6 +16,8 @@ export class Endboss extends MovableObject {
         bottom: 30,
         left: 20,
     };
+    moving = false;
+    attacking = false;
     showFrame = true; // frame for collision implementation
 
     constructor() {
@@ -34,33 +35,36 @@ export class Endboss extends MovableObject {
     }
 
     startMoving() {
-        this.speed = 10;
-        this.moveLeft();
+        if (!this.moving && !this.isDead()) {
+            this.moving = true;
+            IntervalHub.startInterval(this.moveLeft, 1000 / 60);
+        }
     }
 
     attack() {
-        this.speed = 20;
-        this.moveLeft();
+        if (!this.attacking && !this.isDead()) {
+            this.attacking = true;
+            setTimeout(() => (this.attacking = false), 800); // length of the attack animation
+        }
     }
 
     // animate endboss walking
     animate = () => {
-        // this.playAnimation(Imagehub.ENDBOSS.alert);
         if (this.isDead()) {
             this.playAnimation(Imagehub.ENDBOSS.dead);
         } else if (this.isHurt()) {
             this.playAnimation(Imagehub.ENDBOSS.hurt);
-        } else if (this.startMoving) {
-            this.playAnimation(Imagehub.ENDBOSS.move);
-        } else if (this.attack) {
+        } else if (this.attacking) {
             this.playAnimation(Imagehub.ENDBOSS.attack);
+        } else if (this.moving) {
+            this.playAnimation(Imagehub.ENDBOSS.move);
         } else {
             this.playAnimation(Imagehub.ENDBOSS.alert);
         }
     };
 
     // chicken getting hit rreduces energy by 20
-    hit(i) {
+    hit() {
         this.energy -= 20;
         if (this.energy < 0) {
             this.energy = 0;
@@ -70,10 +74,12 @@ export class Endboss extends MovableObject {
     }
 
     removeEnemy(enemy) {
-        if (enemy.isDead())
+        if (enemy.isDead()) {
+            this.speed = 0;
             setTimeout(() => {
                 const index = level1.boss.indexOf(enemy);
                 if (index > -1) level1.boss.splice(index, 1);
             }, 3000);
+        }
     }
 }
