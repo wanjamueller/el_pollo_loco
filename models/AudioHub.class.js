700a6ea8154dbb080014588abc25b8fde82f9cc2
wanjamueller <me@wanjamueller.com>
2026-09-02 15:15:46 +0200

    added and linked all sounds

--- models/AudioHub.class.js
+++ models/AudioHub.class.js
@@ -1,26 +1,46 @@
 // Fixed Audiohub
 
-class MyAudio{
+export class MyAudio {
     file;
     isLoaded;
 
-    constructor(_file){
+    constructor(_file) {
         this.file = new Audio(_file);
     }
 }
 
-
 export class AudioHub {
-    // Audiodateien für Piano, Guitar, DRUMS
-    static PIANO = new MyAudio('./assets/sounds/piano.mp3');
-    static GUITAR = new MyAudio('./assets/sounds/guitar.mp3');
-    static DRUMS = new MyAudio('./assets/sounds/drums.mp3');
+    // audio files for all scenarios
+    static CHARACTER_DAMAGE = new MyAudio(`./assets/audio/character/characterDamage.mp3`);
+    static CHARACTER_DEAD = new MyAudio(`./assets/audio/character/characterDead.wav`);
+    static CHARACTER_JUMP = new MyAudio(`./assets/audio/character/characterJump.wav`);
+    static CHARACTER_RUN = new MyAudio(`./assets/audio/character/characterRun.mp3`);
+    static CHARACTER_SNORING = new MyAudio(`./assets/audio/character/characterSnoring.mp3`);
+    static CHICKEN_DEAD = new MyAudio(`./assets/audio/chicken/chickenDead.mp3`);
+    static CHICKEN_DEAD_2 = new MyAudio(`./assets/audio/chicken/chickenDead2.mp3`);
+    static BOTTLE_COLLECTED = new MyAudio(`./assets/audio/collectibles/bottleCollectSound.wav`);
+    static COIN_COLLECTED = new MyAudio(`./assets/audio/collectibles/collectSound.wav`);
+    static ENDBOSS_APPROACH = new MyAudio(`./assets/audio/endboss/endbossApproach.wav`);
+    static GAME_START = new MyAudio(`./assets/audio/game/gameStart.mp3`);
+    static BOTTLE_BREAK = new MyAudio(`./assets/audio/throwable/bottleBreak.mp3`);
 
-    // Array, das alle definierten Audio-Dateien enthält
-    static allSounds = [AudioHub.PIANO, AudioHub.GUITAR, AudioHub.DRUMS];
+    // Array with all defined audio files
+    static allSounds = [
+        AudioHub.CHARACTER_DAMAGE,
+        AudioHub.CHARACTER_DEAD,
+        AudioHub.CHARACTER_JUMP,
+        AudioHub.CHARACTER_RUN,
+        AudioHub.CHARACTER_SNORING,
+        AudioHub.CHICKEN_DEAD,
+        AudioHub.CHICKEN_DEAD_2,
+        AudioHub.BOTTLE_COLLECTED,
+        AudioHub.COIN_COLLECTED,
+        AudioHub.ENDBOSS_APPROACH,
+        AudioHub.GAME_START,
+        AudioHub.BOTTLE_BREAK,
+    ];
 
-
-    // Spielt eine einzelne Audiodatei ab
+    // plays a specific audio file
     static playOne(sound) {
         sound.file.currentTime = 0;
 
@@ -30,16 +50,14 @@ export class AudioHub {
         }
     }
 
-
-    // Stoppt das Abspielen aller Audiodateien
+    // stops playing all audio files
     static stopAll() {
         AudioHub.allSounds.forEach((sound) => {
             sound.file.pause();
         });
     }
 
-
-    // Stoppt das Abspielen einer einzelnen Audiodatei
+    // stops playing a specific audio file
     static stopOne(sound) {
         sound.file.pause();
     }
